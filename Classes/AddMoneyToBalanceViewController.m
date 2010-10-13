7e38c12f38170327f08716b6898e7ae9c18cefd3
richard-at-netmobo <richard@netmobo.com>
2010-10-13 16:53:55 +0800

    Files updated to include disclaimer

--- Classes/AddMoneyToBalanceViewController.m
+++ Classes/AddMoneyToBalanceViewController.m
@@ -3,8 +3,20 @@
 //  Aggregate
 //
 //  Created by Netmobo on 9/7/10.
-//  Copyright 2010 Aurisoft. All rights reserved.
+//  Copyright 2010 Netmobo. All rights reserved.
 //
+/*
+ Copyright (c) 2010, NETMOBO LLC
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ 
+ Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ Neither the name of NETMOBO LLC nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 
 #import "AddMoneyToBalanceViewController.h"
 #import "AggregateAppDelegate.h"
@@ -176,16 +188,16 @@
 		if ([SKPaymentQueue canMakePayments]) {
 			[model setInAppPurchaseAllowed:@"Y"];
 			
-			NSSet *productIDs = [NSSet setWithObjects:@"com.netmobo.aurisoftdemo.planmovie_USD_1", 
-								 @"com.netmobo.aurisoftdemo.planmovie_USD_2", 
-								 @"com.netmobo.aurisoftdemo.planmovie_USD_3", 
-								 @"com.netmobo.aurisoftdemo.planmovie_USD_4", 
-								 @"com.netmobo.aurisoftdemo.planmovie_USD_5", 
-								 @"com.netmobo.aurisoftdemo.planmovie_USD_6", 
-								 @"com.netmobo.aurisoftdemo.planmovie_USD_7", 
-								 @"com.netmobo.aurisoftdemo.planmovie_USD_8", 
-								 @"com.netmobo.aurisoftdemo.planmovie_USD_9", 
-								 @"com.netmobo.aurisoftdemo.planmovie_USD_10", nil];
+			NSSet *productIDs = [NSSet setWithObjects:@"com.netmobo.Netmobodemo.planmovie_USD_1", 
+								 @"com.netmobo.Netmobodemo.planmovie_USD_2", 
+								 @"com.netmobo.Netmobodemo.planmovie_USD_3", 
+								 @"com.netmobo.Netmobodemo.planmovie_USD_4", 
+								 @"com.netmobo.Netmobodemo.planmovie_USD_5", 
+								 @"com.netmobo.Netmobodemo.planmovie_USD_6", 
+								 @"com.netmobo.Netmobodemo.planmovie_USD_7", 
+								 @"com.netmobo.Netmobodemo.planmovie_USD_8", 
+								 @"com.netmobo.Netmobodemo.planmovie_USD_9", 
+								 @"com.netmobo.Netmobodemo.planmovie_USD_10", nil];
 			
 			SKProductsRequest *request = [[SKProductsRequest alloc] initWithProductIdentifiers:productIDs];
 			request.delegate = self;
--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 //  Aggregate
 //
 //  Created by Netmobo on 9/3/10.
-//  Copyright Aurisoft 2010. All rights reserved.
+//  Copyright Netmobo 2010. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
